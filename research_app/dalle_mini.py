c14e5b57ce414bfddee9fbf6bc1ed2902cc152e2
Aniket Maurya <theaniketmaurya@gmail.com>
2022-06-28 16:08:46 +0530

    Refactor gradio demo (#1)
    * refactor codebase
    
    * refactor gradio codebase
    
    * enable queue

--- research_app/dalle_mini.py
+++ research_app/dalle_mini.py
@@ -21,6 +21,7 @@
 
 # In[1]:
 
+import random
 
 # Model references
 
@@ -40,32 +41,19 @@ VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
 
 import jax
 import jax.numpy as jnp
+from flax.jax_utils import replicate
+from loguru import logger
 
 # check how many devices are available
 jax.local_device_count()
 
 # In[3]:
 
+model, params, vqgan, vqgan_params = [None] * 4
 
 # Load models & tokenizer
 from dalle_mini import DalleBart
 from vqgan_jax.modeling_flax_vqgan import VQModel
-
-# Load dalle-mini
-model, params = DalleBart.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID, dtype=jnp.float16, _do_init=False)
-
-# Load VQGAN
-vqgan, vqgan_params = VQModel.from_pretrained(VQGAN_REPO, revision=VQGAN_COMMIT_ID, _do_init=False)
-
-# Model parameters are replicated on each device for faster inference.
-
-# In[4]:
-
-
-from flax.jax_utils import replicate
-
-params = replicate(params)
-vqgan_params = replicate(vqgan_params)
 
 # Model functions are compiled and parallelized to take advantage of multiple devices.
 
@@ -100,8 +88,6 @@ def p_decode(indices, params):
 # In[6]:
 
 
-import random
-
 # create a random key
 SEED = random.randint(0, 2 ** 32 - 1)
 KEY = jax.random.PRNGKey(SEED)
@@ -119,71 +105,76 @@ processor = DalleBartProcessor.from_pretrained(DALLE_MODEL, revision=DALLE_COMMI
 
 # Let's define some text prompts.
 
-# In[8]:
-
-
-# In[ ]:
-
-
 from flax.training.common_utils import shard_prng_key
 import numpy as np
 from PIL import Image
 from tqdm.notebook import trange
 
 
-def predict(prompt: str):
-    # Note: we could use the same prompt multiple times for faster inference.
-    prompts = [prompt]
-
-    tokenized_prompts = processor(prompts)
-
-    # Finally we replicate the prompts onto each device.
-
-    # In[10]:
-
-
-    tokenized_prompt = replicate(tokenized_prompts)
-
-    # ## 🎨 Generate images
-    #
-    # We generate images using dalle-mini model and decode them with the VQGAN.
-
-    # In[11]:
-
-
-    # number of predictions per prompt
-    n_predictions = 1
-
+class DalleMini:
     # We can customize generation parameters (see https://huggingface.co/blog/how-to-generate)
     gen_top_k = None
     gen_top_p = None
     temperature = None
     cond_scale = 10.0
 
-    print(f"Prompts: {prompts}\n")
-    # generate images
-    images = []
-    for i in trange(max(n_predictions // jax.device_count(), 1)):
-        # get a new key
-        key, subkey = jax.random.split(KEY)
-        # generate images
-        encoded_images = p_generate(
-            tokenized_prompt,
-            shard_prng_key(subkey),
-            params,
-            gen_top_k,
-            gen_top_p,
-            temperature,
-            cond_scale,
-        )
-        # remove BOS
-        encoded_images = encoded_images.sequences[..., 1:]
-        # decode images
-        decoded_images = p_decode(encoded_images, vqgan_params)
-        decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
-        for decoded_img in decoded_images:
-            img = Image.fromarray(np.asarray(decoded_img * 255, dtype=np.uint8))
-            images.append(img)
-            print("created image: ", i)
+    def __init__(self):
+        global model, params, vqgan, vqgan_params
 
-        return images[0]
+        if model is None:
+            # Load dalle-mini
+            model, params = DalleBart.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID, dtype=jnp.float16,
+                _do_init=False)
+
+            # Load VQGAN
+            vqgan, vqgan_params = VQModel.from_pretrained(VQGAN_REPO, revision=VQGAN_COMMIT_ID, _do_init=False)
+
+            # Model parameters are replicated on each device for faster inference.
+
+            params = replicate(params)
+            vqgan_params = replicate(vqgan_params)
+
+        logger.info("created model")
+
+    def predict(self, prompt: str):
+        # Note: we could use the same prompt multiple times for faster inference.
+        prompts = [prompt]
+
+        tokenized_prompts = processor(prompts)
+
+        # Finally we replicate the prompts onto each device.
+        tokenized_prompt = replicate(tokenized_prompts)
+
+        # ## 🎨 Generate images
+        #
+        # We generate images using dalle-mini model and decode them with the VQGAN.
+        # number of predictions per prompt
+        n_predictions = 1
+
+        print(f"Prompts: {prompts}\n")
+        # generate images
+        images = []
+        for i in trange(max(n_predictions // jax.device_count(), 1)):
+            # get a new key
+            key, subkey = jax.random.split(KEY)
+            # generate images
+            encoded_images = p_generate(
+                tokenized_prompt,
+                shard_prng_key(subkey),
+                params,
+                self.gen_top_k,
+                self.gen_top_p,
+                self.temperature,
+                self.cond_scale,
+            )
+            # remove BOS
+            encoded_images = encoded_images.sequences[..., 1:]
+            # decode images
+            decoded_images = p_decode(encoded_images, vqgan_params)
+            decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
+            for decoded_img in decoded_images:
+                img = Image.fromarray(np.asarray(decoded_img * 255, dtype=np.uint8))
+                images.append(img)
+                print("created image: ", i)
+
+            return images[0]
