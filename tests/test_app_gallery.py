0c09a3e8bdf06f9106dbe80d7cd5dbf71cf5b155
manskx <ahmed.mansy156@gmail.com>
2022-08-17 10:38:19 +0200

    fix frontend bug and update tests

--- tests/test_app_gallery.py
+++ tests/test_app_gallery.py
@@ -170,7 +170,7 @@ def validate_app_functionalities(app_page: "Page") -> None:
         try:
             app_page.reload()
             sleep(5)
-            app_label = app_page.frame_locator("iframe").locator("text=CLIP: Connecting")
+            app_label = app_page.frame_locator("iframe").locator("text=DALLE Mini")
             app_label.wait_for(timeout=30 * 1000)
             break
         except (
@@ -179,7 +179,7 @@ def validate_app_functionalities(app_page: "Page") -> None:
         ):
             pass
 
-    app_tabs = ["Blog", "Paper", "Poster", "Notebook", "Training Logs", "Model Demo"]
+    app_tabs = ["Blog", "Paper", "Poster", "Notebook", "Training Logs", "Demo"]
     for tab in app_tabs:
         tab_ui = app_page.locator(f"button:has-text('{tab}')")
         tab_ui.wait_for(timeout=1000)
